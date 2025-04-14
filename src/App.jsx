2949b9b8679903b5570c343ee6df0abc5442eb35
chuks <alexanderchuks310@gmail.com>
2025-04-14 23:42:22 +0100

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Dashboard/>} />
-      <Route path="/assessmentReview" element={<AssessmentReview/>} />
+      <Route exact path="/assessmentreview" element={<AssessmentReview/>} />
     </Routes>
     </BrowserRouter>
     </>
